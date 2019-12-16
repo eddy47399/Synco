aa7357236ae85ed474b66803737f029e57fbf03c
eddy47399 <42643142+eddy47399@users.noreply.github.com>
2019-12-16 08:54:14 +0100

    Add files via upload

---
+++ Synco/src/main/kotlin/com/synco/Synco/SyncoApplication.kt
@@ -0,0 +1,11 @@
+package com.synco.Synco
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SyncoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SyncoApplication>(*args)
+}
---
+++ Synco/src/test/kotlin/com/synco/Synco/SyncoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.synco.Synco
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SyncoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
